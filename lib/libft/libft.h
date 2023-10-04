896fa4907c1fb75eb08381b43446bc6086eea14d
Ulysse LEVALLOIS <ulevallo@bess-f3r2s11.clusters.42paris.fr>
2023-10-04 18:16:16 +0200

    [EDIT] include files and libft headers [ADD] ft_free_dstr to libft environment functions within minishell.c

--- lib/libft/libft.h
+++ lib/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:17:28 by ulevallo          #+#    #+#             */
-/*   Updated: 2023/10/04 15:51:41 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:55:59 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,5 +54,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_free_dstr(char **dstr);
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,32 +6,77 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:06:16 by aoberon           #+#    #+#             */
-/*   Updated: 2023/10/04 16:00:42 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/10/04 18:10:08 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+/**
+ * @brief Copies the inherited env object
+ * 			so that the shell can manage it's own environement
+ *
+ * @param old_env
+ * @return the copied environment
+ */
+char	**copy_env(char **old_env)
 {
-	printf("minishell :\n");
-	if (argc == 1)
+	char	**new_env;
+	size_t	size;
+	size_t	i;
+
+	size = 0;
+	while (old_env && old_env[size])
+		size++;
+	if (size == 0)
+		return (NULL);
+	new_env = ft_calloc(size + 1, sizeof(char *));
+	if (!new_env)
+		return (NULL);
+	i = 0;
+	while (old_env[i])
 	{
-		printf("No arguments.\n");
-		return (1);
+		new_env[i] = ft_strdup(old_env[i]);
+		if (!new_env[i])
+			return (ft_free_dstr(new_env), NULL);
+		i++;
 	}
-	if (detect_echo(argc - 1, argv + 1))
-		return (0);
-	if (detect_cd(argv + 1))
-		return (0);
-	if (detect_pwd(argv[1]))
-		return (0);
-	if (detect_exit(argv[1]))
-		return (0);
+	return (new_env);
+}
+
+/**
+ * @brief creates a default shell environment creating 3 default variables
+ *
+ * /!\ UNFINISHED
+ *
+ * @return returns a brand new allocated env
+ */
+char	**create_default_env(void)
+{
+	char	**env;
+
+	env = ft_calloc(3 + 1, sizeof(char *));
+	env[0] = ft_strdup("PWD=");
+	env[1] = ft_strdup("SHLVL=");
+	env[2] = ft_strdup("_=");
+	return (new_env);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	**env;
+
+	(void)argc;
+	(void)argv;
+	if (envp)
+		env = copy_env(envp);
 	else
+		env = create_default_env();
+	if (!env)
+		return (EXIT_FAILURE);
+	for (size_t i = 0; env && env[i]; i++)
 	{
-		printf("Not a builtins.\n");
-		return (1);
+		printf ("The %zu str is: %s\n", i, env[i]);
 	}
 	return (0);
 }
