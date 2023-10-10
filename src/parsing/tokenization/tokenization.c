4d075c34f8bf995b81e9d7a2f21418d27ce17753
Arthur Oberon <oberon.arthur42@gmail.com>
2023-10-10 11:06:50 +0200

    [EDIT] tokenization (add redir > between arg)

--- src/parsing/tokenization/tokenization.c
+++ src/parsing/tokenization/tokenization.c
@@ -6,7 +6,7 @@
 /*   By: aoberon <aoberon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 17:06:06 by aoberon           #+#    #+#             */
-/*   Updated: 2023/10/09 18:27:58 by aoberon          ###   ########.fr       */
+/*   Updated: 2023/10/10 11:04:00 by aoberon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,19 @@ static size_t	count_tokenization(char **argv)
 			count++;
 		}
 		else
-			i++;
+		{
+			while (argv[i] && ft_strcmp(argv[i], "|"))
+			{
+				if (!ft_strcmp(argv[i], "<") || !ft_strcmp(argv[i], ">")
+					|| !ft_strcmp(argv[i], ">>"))
+				{
+					i += 2;
+					count += 2;
+				}
+				else
+					i++;
+			}
+		}
 	}
 	printf("count: %d\n", count);
 	return (count);
@@ -42,19 +54,46 @@ static size_t	count_tokenization(char **argv)
 size_t	get_next_command_type(char **argv)
 {
 	int	i;
+	int	count;
 
 	i = 0;
-	while (argv[i] && ft_strcmp(argv[i], "<") && ft_strcmp(argv[i], "<<")
-		&& ft_strcmp(argv[i], ">") && ft_strcmp(argv[i], ">>")
-		&& ft_strcmp(argv[i], "|"))
+	count = 0;
+	while (argv[i] && ft_strcmp(argv[i], "|"))
+	{
+		if (!ft_strcmp(argv[i], ">"))
+		{
+			i += 2;
+		}
+		else
+		{
+			i++;
+			count++;
+		}
+	}
+	return (count);
+}
+
+char	**doublecharncpy(char **src, size_t n)
+{
+	char	**dest;
+	size_t	i;
+
+	dest = ft_calloc(n + 1, sizeof(char *));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = ft_strdup(src[i]);
 		i++;
-	return (i);
+	}
+	return (dest);
 }
 
 t_token	*tokenization(char **argv)
 {
-	int		i;
-	int		n;
+	size_t	i;
+	size_t	n;
 	t_token	*token;
 
 	token = ft_calloc(count_tokenization(argv) + 1, sizeof(t_token));
@@ -67,91 +106,79 @@ t_token	*tokenization(char **argv)
 		if (!ft_strcmp(argv[i], "<"))
 		{
 			token[n].type = REDIR_IN;
-			token[n].content = ft_calloc(3, sizeof(char *));
+			token[n].content = doublecharncpy(&argv[i], 2);
 			if (!token[n].content)
 				return (printf("FREE AND EXIT !\n"), NULL);
-			token[n].content[0] = ft_strdup(argv[i]);
-			i++;
-			token[n].content[1] = ft_strdup(argv[i]);
-			token[n].content[2] = NULL;
-			i++;
+			i += 2;
 			n++;
 		}
 		else if (!ft_strcmp(argv[i], ">"))
 		{
 			token[n].type = REDIR_OUT;
-			token[n].content = ft_calloc(3, sizeof(char *));
+			token[n].content = doublecharncpy(&argv[i], 2);
 			if (!token[n].content)
 				return (printf("FREE AND EXIT !\n"), NULL);
-			token[n].content[0] = ft_strdup(argv[i]);
-			i++;
-			token[n].content[1] = ft_strdup(argv[i]);
-			token[n].content[2] = NULL;
-			i++;
+			i += 2;
 			n++;
 		}
 		else if (!ft_strcmp(argv[i], "<<"))
 		{
 			token[n].type = HERE_DOC;
-			token[n].content = ft_calloc(3, sizeof(char *));
+			token[n].content = doublecharncpy(&argv[i], 2);
 			if (!token[n].content)
 				return (printf("FREE AND EXIT !\n"), NULL);
-			token[n].content[0] = ft_strdup(argv[i]);
-			i++;
-			token[n].content[1] = ft_strdup(argv[i]);
-			token[n].content[2] = NULL;
-			i++;
+			i += 2;
 			n++;
 		}
 		else if (!ft_strcmp(argv[i], ">>"))
 		{
 			token[n].type = APPEND;
-			token[n].content = ft_calloc(3, sizeof(char *));
+			token[n].content = doublecharncpy(&argv[i], 2);
 			if (!token[n].content)
 				return (printf("FREE AND EXIT !\n"), NULL);
-			token[n].content[0] = ft_strdup(argv[i]);
-			i++;
-			token[n].content[1] = ft_strdup(argv[i]);
-			token[n].content[2] = NULL;
-			i++;
+			i += 2;
 			n++;
 		}
 		else if (!ft_strcmp(argv[i], "|"))
 		{
 			token[n].type = PIPE;
-			token[n].content = ft_calloc(2, sizeof(char *));
+			token[n].content = doublecharncpy(&argv[i], 1);
 			if (!token[n].content)
 				return (printf("FREE AND EXIT !\n"), NULL);
-			token[n].content[0] = ft_strdup(argv[i]);
-			i++;
-			token[n].content[2] = NULL;
 			i++;
 			n++;
 		}
 		else
 		{
-			printf("i: %d\n", i);
-			printf("argv[i]: %s\n", argv[i]);
-			printf("n: %d\n", n);
-			int		j;
-			int		k;
+			size_t		j;
+			size_t		k;
+			size_t	n_bis;
 			token[n].type = CMD;
 			j = get_next_command_type(&argv[i]);
-			printf("j: %d\n", j);
 			token[n].content = ft_calloc(j + 1, sizeof(char *));
 			if (!token[n].content)
 				return (printf("FREE AND EXIT !\n"), NULL);
 			k = 0;
+			n_bis = 0;
 			while (k < j)
 			{
-				printf("strdup: %s\n", argv[i]);
-				token[n].content[k] = ft_strdup(argv[i]);
-				i++;
+				if (!ft_strcmp(argv[i], ">"))
+				{
+					n_bis++;
+					token[n + n_bis].type = REDIR_OUT;
+					token[n + n_bis].content = doublecharncpy(&argv[i], 2);
+					if (!token[n + n_bis].content)
+						return (printf("FREE AND EXIT !\n"), NULL);
+					i += 2;
+				}
+				if (argv[i])
+				{
+					token[n].content[k] = ft_strdup(argv[i]);
+					i++;
+				}
 				k++;
 			}
-			printf("end loop : %s\n", argv[i]);
-			// i++;
-			n++;
+			n += n_bis + 1;
 		}
 	}
 	debug_token(token);
