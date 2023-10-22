355edd3d9ea4dd264aca17534fac40a78b1736df
Ulysse LEVALLOIS <ulevallo@made-f0Ar7s7.clusters.42paris.fr>
2023-10-22 17:29:34 +0200

    [EDIT] from execution implementation

--- include/execution/execution.h
+++ include/execution/execution.h
@@ -6,7 +6,7 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:20:51 by aoberon           #+#    #+#             */
-/*   Updated: 2023/10/13 21:40:36 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/10/22 17:05:05 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,18 @@
 
 void	prompt(char **envp);
 t_token	*parse_line(char *line);
+
+// ========================= EXECUTION ========================
+
 int		execute(t_token *tokens, char **env);
+
+typedef struct s_pipes	t_pipes;
+
+struct s_pipes
+{
+	int	pids[1024];
+	int	s[2];
+	int	prev;
+};
 
 #endif
--- src/execution/execution.c
+++ src/execution/execution.c
@@ -6,41 +6,38 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 21:31:47 by ulevallo          #+#    #+#             */
-/*   Updated: 2023/10/22 17:18:44 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/10/22 17:28:32 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // TODO: actually code the function please
-int	execute(t_token *tokens, char **env)
-{
-	int	i;
-
-	(void)env;
-	i = 0;
-	while (tokens && i < 2)
-	{
-		i++;
-	}
-	return (1);
-}
-
-/**
- * @brief char	**get_env_path(char **env)
+int	execute(t_token *token_list, char **env)
 {
 	int		i;
+	t_pipes	my_pipe;
 
 	i = 0;
-	while (env && env[i])
+	while (token_list && token_list[i])
 	{
-		if (!ft_strncmp(env[i], PATH, PATH_SIZE))
-			return (ft_split(&env[i][5], PATH_ENV_SEPARATOR));
+		if () // not last cmd
+			pipe(my_pipe.s);
+		pipex.pids[i] = fork(); // store the pid
+		if (pipex.pids[i] == 0) // if inside the child do the process
+			child_process(&pipex, av[2 + i], i); // start the child loop
+		if (pipex.prev_pipe != -1) // if there is a previous pipe close it
+			close(pipex.prev_pipe);
+		pipex.prev_pipe = pipex.zelda[0]; // store the new previous pipe for the next child process to use
+		close(pipex.zelda[1]);
 		i++;
 	}
-	return (NULL);
+	(close(pipex.zelda[0]), close(pipex.prev_pipe)); // close ur unclosed pipes
+	free_dstr(pipex.path);
+	pipex.pids[i] = -1;
+	return (wait_pids(pipex.pids));
 }
-
+/*
 int	wait_pids(int *pids)
 {
 	int	i;
@@ -55,44 +52,4 @@ int	wait_pids(int *pids)
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
 	return (1);
-}
-
-void	init_pipex(t_pipex *pipex, int ac, char **av, char **env)
-{
-	pipex->env = env;
-	pipex->size = ac - 3;
-	pipex->infile = av[1];
-	pipex->outfile = av[ac - 1];
-	pipex->path = get_env_path(env);
-	pipex->prev_pipe = -1;
-}
-
-int	main(int ac, char **av, char **env)
-{
-	t_pipex	pipex;
-	int		i;
-
-	if (ac < 5 || ac > 1026)
-		return (ft_printf("not [enough/too_many] arguments\n"), EXIT_FAILURE);
-	init_pipex(&pipex, ac, av, env);
-	i = 0;
-	while (i < (ac - 3))
-	{
-		if (i < (ac - 4))
-			pipe(pipex.zelda);
-		pipex.pids[i] = fork();
-		if (pipex.pids[i] == 0)
-			child_process(&pipex, av[2 + i], i);
-		if (pipex.prev_pipe != -1)
-			close(pipex.prev_pipe);
-		pipex.prev_pipe = pipex.zelda[0];
-		close(pipex.zelda[1]);
-		i++;
-	}
-	close(pipex.zelda[0]);
-	close(pipex.prev_pipe);
-	free_dstr(pipex.path);
-	pipex.pids[i] = -1;
-	return (wait_pids(pipex.pids));
-}
- */
+}*/
