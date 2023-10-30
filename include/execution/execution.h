38d59f989ec5461ae7112f6147504ab634b23c5d
UrNot42 <ulevallo@student.42.fr>
2023-10-30 19:36:55 +0100

    "[FIX] exec.h"

--- include/execution/execution.h
+++ include/execution/execution.h
@@ -6,7 +6,7 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:20:51 by ulevallo          #+#    #+#             */
-/*   Updated: 2023/10/30 17:55:54 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/10/30 19:35:19 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@
 # include "exec_struct.h"
 # include "files.h"
 # include "here_doc.h"
-
-# define
 
 // ========================== PROMPT ==========================
 
