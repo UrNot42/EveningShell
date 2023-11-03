c78bc042414706db6696bdc81fefa4719b18c132
Arthur Oberon <oberon.arthur42@gmail.com>
2023-11-03 18:51:33 +0100

    [ADD] separated by special character on expand

--- src/expand/check_for_dollar.c
+++ src/expand/check_for_dollar.c
@@ -6,11 +6,28 @@
 /*   By: aoberon <aoberon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 17:35:53 by aoberon           #+#    #+#             */
-/*   Updated: 2023/11/02 10:45:45 by aoberon          ###   ########.fr       */
+/*   Updated: 2023/11/03 18:50:53 by aoberon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief Check if the char c is a special character
+ * 
+ * @param c char to check
+ * @return int 1 if c is a special character, 0 otherwise
+ */
+static int	is_special_character(int c)
+{
+	if (c == ' ' || c == '\t' || c == '\'' || c == '"'
+		|| c == '$' || c == '{' || c == '}' || c == '~' || c == '`'
+		|| c == '&' || c == '?' || c == ';' || c == '(' || c == ')'
+		|| c == '[' || c == ']' || c == '*' || c == '+' || c == '-'
+		|| c == '@' || c == ':' || c == '.' || c == ',' || c == '/')
+		return (1);
+	return (0);
+}
 
 /**
  * @brief Moving the index_dollar to the end of the single quotes string
@@ -48,9 +65,10 @@ static size_t	dollar_in_double_quotes(char const *str, size_t *index_dollar)
 		if (str[*index_dollar] == '$')
 		{
 			*index_dollar += 1;
-			while (str[*index_dollar] && str[*index_dollar] != ' '
-				&& str[*index_dollar] != '\t' && str[*index_dollar] != '\''
-				&& str[*index_dollar] != '"')
+			while (str[*index_dollar]
+				&& !is_special_character(str[*index_dollar])
+				&& str[*index_dollar] != '|'
+				&& str[*index_dollar] != '<' && str[*index_dollar] != '>')
 			{
 				*index_dollar += 1;
 				expand_length++;
@@ -87,10 +105,7 @@ static size_t	dollar_alone(char const *str, size_t *index_dollar)
 	{
 		return (0);
 	}
-	while (str[*index_dollar]
-		&& str[*index_dollar] != ' ' && str[*index_dollar] != '\t'
-		&& str[*index_dollar] != '\'' && str[*index_dollar] != '"'
-		&& str[*index_dollar] != '$')
+	while (str[*index_dollar] && !is_special_character(str[*index_dollar]))
 	{
 		*index_dollar += 1;
 		expand_length++;
