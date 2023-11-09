1aff0b98fd16339d2951e2621180eb87ab113eb5
Arthur Oberon <oberon.arthur42@gmail.com>
2023-11-09 14:06:26 +0100

    [ADD] Start new expand (dollar + quotes + split)

--- src/expand/expand_one_compound_new.c
+++ src/expand/expand_one_compound_new.c
@@ -6,81 +6,81 @@
 /*   By: aoberon <aoberon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 15:23:20 by aoberon           #+#    #+#             */
-/*   Updated: 2023/11/08 17:13:40 by aoberon          ###   ########.fr       */
+/*   Updated: 2023/11/09 14:05:13 by aoberon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	copy_var_content(char **new_content, char *var_content, size_t *i)
-{
-	size_t	n;
+// static void	copy_var_content(char **new_content, char *var_content, size_t *i)
+// {
+// 	size_t	n;
 
-	n = 0;
-	while (var_content && var_content[n])
-	{
-		(*new_content)[*i] = var_content[n++];
-		*i += 1;
-	}
-}
+// 	n = 0;
+// 	while (var_content && var_content[n])
+// 	{
+// 		(*new_content)[*i] = var_content[n++];
+// 		*i += 1;
+// 	}
+// }
 
-static	char	*merge_var_content(char *src, t_expand_params params)
-{
-	char	*new_content;
-	size_t	i;
-	size_t	j;
-	size_t	size;
+// static	char	*merge_var_content(char *src, t_expand_params params)
+// {
+// 	char	*new_content;
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	size;
 
-	size = (ft_strlen(src) - params.var_name_length) + ft_strlen(params.var);
-	new_content = ft_calloc(size + 1, sizeof(char));
-	if (!new_content)
-		return (free(src), free(params.var), NULL);
-	i = 0;
-	j = 0;
-	while (i < size)
-	{
-		if (i == params.index_dollar)
-		{
-			j += params.var_name_length + 1;
-			if (params.var)
-				copy_var_content(&new_content, params.var, &i);
-			else
-				new_content[i++] = src[j++];
-		}
-		else
-			new_content[i++] = src[j++];
-		// printf("new_content: %s\n", new_content);
-	}
-	return (free(src), free(params.var), new_content);
-}
+// 	size = (ft_strlen(src) - params.var_name_length) + ft_strlen(params.var);
+// 	new_content = ft_calloc(size + 1, sizeof(char));
+// 	if (!new_content)
+// 		return (free(src), free(params.var), NULL);
+// 	i = 0;
+// 	j = 0;
+// 	while (i < size)
+// 	{
+// 		if (i == params.index_dollar)
+// 		{
+// 			j += params.var_name_length + 1;
+// 			if (params.var)
+// 				copy_var_content(&new_content, params.var, &i);
+// 			else
+// 				new_content[i++] = src[j++];
+// 		}
+// 		else
+// 			new_content[i++] = src[j++];
+// 		// printf("new_content: %s\n", new_content);
+// 	}
+// 	return (free(src), free(params.var), new_content);
+// }
 
-// ====================================================================
+// // ====================================================================
 
-static void	debug_expand(char *content, int index)
-{
-	int	i;
+// static void	debug_expand(char *content, int index)
+// {
+// 	int	i;
 
-	i = 0;
-	printf("content: ");
-	while (content[i])
-	{
-		if (i == index)
-		{
-			if (content[i] == '"' || content[i] == '\'')
-				printf("\033[0;31m%c\033[0;37m", content[i]);
-			else if (content[i] == '$')
-				printf("\033[0;32m%c\033[0;37m", content[i]);
-			else
-				printf("\033[0;36m%c\033[0;37m", content[i]);
-		}
-		else
-			printf("%c", content[i]);
-		++i;
-	}
-	printf("\n");
-}
+// 	i = 0;
+// 	printf("content: ");
+// 	while (content[i])
+// 	{
+// 		if (i == index)
+// 		{
+// 			if (content[i] == '"' || content[i] == '\'')
+// 				printf("\033[0;31m%c\033[0;37m", content[i]);
+// 			else if (content[i] == '$')
+// 				printf("\033[0;32m%c\033[0;37m", content[i]);
+// 			else
+// 				printf("\033[0;36m%c\033[0;37m", content[i]);
+// 		}
+// 		else
+// 			printf("%c", content[i]);
+// 		++i;
+// 	}
+// 	printf("\n");
+// }
 
-// ====================================================================
+// // ====================================================================
 
 static int	is_special_character(int c)
 {
@@ -93,7 +93,7 @@ static int	is_special_character(int c)
 	return (0);
 }
 
-static size_t	get_varname_length(char const *str, int	index)
+static size_t	get_varname_length(char const *str, int index)
 {
 	size_t	length;
 
@@ -102,7 +102,6 @@ static size_t	get_varname_length(char const *str, int	index)
 	if (!ft_isalpha(str[index]) && str[index] != '_'
 		&& ft_isdigit(str[index]))
 	{
-		++index;
 		return (1);
 	}
 	else if (!ft_isalpha(str[index]) && str[index] != '_'
@@ -110,9 +109,11 @@ static size_t	get_varname_length(char const *str, int	index)
 	{
 		return (0);
 	}
-	while (str[index]
-		&& ((index == 1 && str[index] == '?')
-			|| !is_special_character(str[index])))
+	else if (str[index] == '?')
+	{
+		return (1);
+	}
+	while (str[index] && !is_special_character(str[index]))
 	{
 		++index;
 		++length;
@@ -120,35 +121,126 @@ static size_t	get_varname_length(char const *str, int	index)
 	return (length);
 }
 
+// // ====================================================================
+
+// char	*remove_one_couple_single_quotes(char *content, int *index)
+// {
+// 	char	*new_content;
+// 	int		i;
+// 	int		j;
+// 	int		count_quotes;
+
+// 	new_content = ft_calloc(ft_strlen(content) - 2, sizeof(char));
+// 	if (!new_content)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	count_quotes = 0;
+// 	while (content[i])
+// 	{
+// 		if (i == *index && content[i] == '\'')
+// 			++count_quotes;
+// 		else if (i > *index && count_quotes == 1 && content[i] == '\'')
+// 		{
+// 			*index = i + 1;
+// 			++count_quotes;
+// 		}
+// 		else
+// 			new_content[j++] = content[i];
+// 		++i;
+// 	}
+// 	return (new_content);
+// }
+
 // ====================================================================
 
-char	*remove_one_couple_single_quotes(char *content, int *index)
+static	int	retrieve_var_name(char **var, char *str,
+	size_t index_dollar, size_t size)
 {
-	char	*new_content;
-	int		i;
-	int		j;
-	int		count_quotes;
+	size_t	i;
 
-	new_content = ft_calloc(ft_strlen(content) - 2, sizeof(char));
-	if (!new_content)
-		return (NULL);
 	i = 0;
-	j = 0;
-	count_quotes = 0;
-	while (content[i])
+	*var = ft_calloc(size + 1, sizeof(char));
+	if (!*var)
+		return (0);
+	++index_dollar;
+	while (i < size)
 	{
-		if (i == *index && content[i] == '\'')
-			++count_quotes;
-		else if (i > *index && count_quotes == 1 && content[i] == '\'')
-		{
-			*index = i + 1;
-			++count_quotes;
-		}
-		else
-			new_content[j++] = content[i];
+		(*var)[i] = str[index_dollar];
+		++index_dollar;
 		++i;
 	}
-	return (new_content);
+	return (1);
+}
+
+int	expand_dollar(char **env, char *content, int *index, int exit_status, int *count)
+{
+	char	*var_name;
+	size_t	var_name_length;
+	size_t	i;
+	size_t	j;
+	int		index_env;
+	char	flag;
+
+	var_name_length = get_varname_length(content, *index);
+	if (!retrieve_var_name(&var_name, content, *index, var_name_length))
+		return (-1);
+	j = 0;
+	while (j++ < var_name_length)
+	{
+		*index += 1;
+	}
+	if (!ft_strcmp(var_name, "?"))
+	{
+		printf("%d", exit_status);
+		return (1);
+	}
+	index_env = get_env_var_index(env, var_name);
+	i = 0;
+	if (index_env != -1)
+	{
+		while (env[index_env][i] != '=')
+			++i;
+		++i;
+		flag = '\0';
+		while (env[index_env][i])
+		{
+			printf("%c", env[index_env][i]);
+			if ((env[index_env][i] == ' ' || env[index_env][i] == '\t'))
+			{
+				*count += 1;
+				printf("\n");
+			}
+			if (env[index_env][i] == '\'')
+			{
+				if (flag == '\0')
+				{
+					flag = '\'';
+				}
+				else if (flag == '\'')
+				{
+					flag = '\0';
+					*count += 1;
+					printf("\n");
+				}
+			}
+			else if (env[index_env][i] == '"')
+			{
+				if (flag == 0)
+				{
+					flag = '"';
+				}
+				else if (flag == '"')
+				{
+					flag = '\0';
+					*count += 1;
+					printf("\n");
+				}
+			}
+			++i;
+		}
+	}
+	return (1);
 }
 
 // ====================================================================
@@ -156,37 +248,73 @@ char	*remove_one_couple_single_quotes(char *content, int *index)
 int	expand_one_content(char **content, char **env, int exit_status)
 {
 	int				index;
-	t_expand_params	params;
+	int				count;
+	char			flag;
+	// t_expand_params	params;
 
 	index = 0;
+	count = 0;
+	flag = '\0';
 	while ((*content)[index])
 	{
-		debug_expand(*content, index);
-		// if ((*content)[index] == '"')
-		// {
-		// 	;
-		// }
-		if ((*content)[index] == '\'')
+		// debug_expand(*content, index);
+		if (((*content)[index] == ' ' || (*content)[index] == '\t'))
 		{
-			++index;
-			while ((*content)[index] != '\'')
-				++index;
-			++index;
+			++count;
+			printf("\n");
+		}
+		else if ((*content)[index] == '\'')
+		{
+			if (flag == '\0')
+			{
+				flag = '\'';
+			}
+			else if (flag == '\'')
+			{
+				flag = '\0';
+				++count;
+				printf("\n");
+			}
+			else if (flag == '"')
+			{
+				printf("%c", (*content)[index]);
+			}
+		}
+		else if ((*content)[index] == '"')
+		{
+			if (flag == 0)
+			{
+				flag = '"';
+			}
+			else if (flag == '\'')
+			{
+				printf("%c", (*content)[index]);
+			}
+			else if (flag == '"')
+			{
+				flag = '\0';
+				++count;
+				printf("\n");
+			}
 		}
 		else if ((*content)[index] == '$')
 		{
-			params.var_name_length = get_varname_length(*content, index);
-			params.index_dollar = index;
-			if (set_var(&params, *content, env, exit_status) == -1)
-				return (-1);
-			*content = merge_var_content(*content, params);
-			if (!content)
-				return (-1);
-			index += params.var_content_length;
+			if (flag == '\'')
+			{
+				printf("%c", (*content)[index]);
+			}
+			else if (flag == 0 || flag == '"')
+			{
+				if (expand_dollar(env, *content, &index, exit_status, &count) == -1)
+					return (-1);
+			}
 		}
 		else
-			++index;
+			printf("%c", (*content)[index]);
+		++index;
 	}
+	printf("\ncount : [%d]\n", count);
+	printf("------------------------------\n");
 	return (1);
 }
 
@@ -205,3 +333,16 @@ int	expand_one_compound(t_compound compound, char **env, int exit_status)
 
 // echo $HOME"$USER"'$PATH'"'$?'"$TOTO'"$PATH"'
 // /mnt/nfs/homes/aoberonaoberon$PATH'0'"$PATH"
+
+// echo "toto$HOME"'$USER toto'"'toto'"
+
+
+// bash-5.1$ '$HOME'$USER
+// bash: $HOMEaoberon: command not found
+
+// minishell-0.1$ '$HOME'$USER
+// $HOME
+// aoberon
+// count : [1]
+// ------------------------------
+// Command ''$HOME'$USER' not found
