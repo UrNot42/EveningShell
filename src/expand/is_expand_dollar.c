6ddd33111a128651912518aaf400b8d29c4fb089
Arthur Oberon <oberon.arthur42@gmail.com>
2023-11-12 22:32:51 +0100

    [FIX] error on is_expand_dollar()

--- src/expand/is_expand_dollar.c
+++ src/expand/is_expand_dollar.c
@@ -6,7 +6,7 @@
 /*   By: aoberon <aoberon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 21:34:33 by aoberon           #+#    #+#             */
-/*   Updated: 2023/11/12 21:36:52 by aoberon          ###   ########.fr       */
+/*   Updated: 2023/11/12 22:26:49 by aoberon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static bool	is_expand_dollar_single_quote(char *word, int *i)
 {
 	*i += 1;
 	while (word[*i] && word[*i] != '\'')
-		++i;
+		*i += 1;
 	if (!word[*i])
 		return (false);
 	return (true);
@@ -29,7 +29,7 @@ static bool	is_expand_dollar_double_quote(char *word, int *i)
 	{
 		if (word[*i] == '$')
 			return (true);
-		++i;
+		*i += 1;
 	}
 	if (!word[*i])
 		return (false);
