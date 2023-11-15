0f9e0257bd26dda4ca23e995082c0e6dfde4b917
UrNot42 <ulevallo@student.42.fr>
2023-11-15 18:14:42 +0100

    [FIX] WAITS FOR HEREDOC TO SEND -130 WHEN CTRL-C

--- src/execution/overlay/file_handling.c
+++ src/execution/overlay/file_handling.c
@@ -6,7 +6,7 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 21:06:23 by ulevallo          #+#    #+#             */
-/*   Updated: 2023/11/06 20:37:23 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/11/15 18:14:03 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,15 @@ int	open_here_documents(t_file *file, t_exec *exec)
 	{
 		if (file[i].type == HERE_DOC)
 			file[i].fd = heredoc(exec, file[i].name);
-		if (file[i].type == HERE_DOC && file[i].fd == 1)
+		if (file[i].type == HERE_DOC && file[i].fd < 0)
 		{
+			i = file[i].fd;
+			if (i == -130)
+				g_signal = 130;
+			else if (i == -42)
+				perror("heredoc");
+			if (i == -42)
+				(error_failed_malloc(), g_signal = -42);
 			close_files(file, i);
 			return (perror(file[i].name), i);
 		}
