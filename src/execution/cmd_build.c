2151d11d43159a06a936ef8d4a841749f4e97799
UrNot42 <ulevallo@student.42.fr>
2023-11-16 22:45:22 +0100

    [NORM]

--- src/execution/cmd_build.c
+++ src/execution/cmd_build.c
@@ -6,7 +6,7 @@
 /*   By: ulevallo <ulevallo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 10:15:05 by ulevallo          #+#    #+#             */
-/*   Updated: 2023/11/16 20:33:12 by ulevallo         ###   ########.fr       */
+/*   Updated: 2023/11/16 22:44:58 by ulevallo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ char	*get_cmd(char *cmd, char *path, int *err)
 			return (ft_strdup(cmd));
 		if (cmd[i++] == '/' && access(cmd, F_OK | X_OK))
 			return (perror(cmd), (access(cmd, X_OK) && ++*err),
-			(access(cmd, F_OK) && --*err), NULL);
+				(access(cmd, F_OK) && --*err), NULL);
 	}
 	while (path && *path)
 	{
