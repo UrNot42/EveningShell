f44003281bf2d6ae656de33a899258268b92b834
Arthur Oberon <oberon.arthur42@gmail.com>
2023-11-19 16:55:17 +0100

    [EDIT] cd - print working directory after moving)

--- src/execution/builtins/cd.c
+++ src/execution/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: aoberon <aoberon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 17:36:57 by aoberon           #+#    #+#             */
-/*   Updated: 2023/11/19 11:46:55 by aoberon          ###   ########.fr       */
+/*   Updated: 2023/11/19 16:53:54 by aoberon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	builtins_cd(char **args, char **env)
 			(write(2, "MarmiShell: cd: OLDPWD not set\n", 31), ++err);
 		else if (chdir(&env[index][7]) == -1)
 			return (perror("cd"), err);
+		builtins_pwd();
 	}
 	else if (args[2] == NULL && chdir(args[1]) == -1)
 		return (perror("cd"), ++err);
